40c858bb2fe73ef42c52d9cbe459c05baea5c5b4
Umiddev1 <umidjanrahmanjanov@gmail.com>
2022-01-24 09:40:16 +0500

    end pr

--- src/Components/ContactBtn.js
+++ src/Components/ContactBtn.js
@@ -1,23 +1,19 @@
 import React from 'react';
 
 function ContactBtn({todos, setTodos}) {
-  function family() {
-   const filter = todos.filter((items) =>  items.relation === "Family")
-   setTodos(filter);
-  }
-  function freind() {
-    const filter = todos.filter((items) => items.relation === "Freinds")
-    setTodos(filter)
-  }
-  function relation() {
-    const filter = todos.filter((items) => items.relation === "Relatives")
-    setTodos(filter)
+  function family(text) {
+    console.log(text);
+    if(text == "All")setTodos(todos)
+   const filter = todos.filter((items) => items.relation == text);
+   if(filter.length > 0 && text != "All")setTodos(filter);
+   if(filter.length == 0 && text != "All")setTodos([]);
   }
   return (
     <div className="btn-group">
-     <button type="button" onClick={family} className="btn btn-primary bt">Family</button>
-     <button type="button" onClick={freind} className="btn btn-secondary bt">Freinds</button>
-     <button type="button" onClick={relation} className="btn btn-success bt">Relationship</button>
+     <button type="button" onClick={() => family('All')} className="btn btn-dark bt">All</button>
+     <button type="button" onClick={() => family('Family')} className="btn btn-primary bt">Family</button>
+     <button type="button" onClick={() => family('Freinds')} className="btn btn-secondary bt">Freinds</button>
+     <button type="button" onClick={() => family('Relatives')} className="btn btn-success bt">Relationship</button>
    </div>
   )
 }
